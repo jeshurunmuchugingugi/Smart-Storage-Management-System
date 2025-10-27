37ecb5d6b5ca5099188901c7ec013fc4275ebdd4
ivy <ivy.chemutai@student.moringaschool.com>
2025-10-27 08:55:16 +0300

    save changes

--- client/src/App.js
+++ client/src/App.js
@@ -34,7 +34,9 @@ const App = () => {
               <Header />
               <HeroSection />
               <Services />
-              <Footer />
+              <HowItWorks/>
+              <About />
+              < Footer />
             </>
           } />
         </Routes>
